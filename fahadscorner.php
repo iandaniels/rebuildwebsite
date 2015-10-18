a01f51f21f9f46e47888a90591b601af42714a79
Ian Daniels <ian.daniels@hp.com>
2015-10-18 21:52:11 +0100

    Add Fahad's corner
    Add Fahad's corner page.

---
+++ fahadscorner.php
@@ -0,0 +1,181 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <!-- Custom fonts -->
+    <script src="https://use.typekit.net/jim6vhf.js"></script>
+    <script>try { Typekit.load({ async: true }); } catch (e) { }</script>
+
+    <meta name="description" content="The UCL Law Society">
+    <meta name="author" content="The UCL Law Society">
+    <!--<link rel="icon" href="favicon.ico">-->
+    <link rel="icon" type="image/png" href="/images/icon.png" />
+
+    <title>The UCL Law Society</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/custom-styles.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.min.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+    <!-- Static navbar -->
+    <nav class="navbar navbar-inverse navbar-static-top">
+        <div class="container">
+            <div class="text-center">
+                <h3><img alt="UCL Law Soc" src="images/lawsoclogo.png" /></h3>
+            </div>
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <!--<a class="navbar-brand" href="#">Project name</a>-->
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li><a href="index.php">HOME</a></li>
+                    <li><a href="#">NEWS</a></li>
+                    <li><a href="socials.php">SOCIALS</a></li>
+                    <li><a href="careers.php">CAREERS</a></li>
+                    <li><a href="barcareers.php">BAR CAREERS</a></li>
+                    <li><a href="competitions.php">COMPETITIONS</a></li>
+                    <li><a href="silkyb.php">SILK v BRIEF</a></li>
+                    <li><a href="sports.php">SPORTS</a></li>
+                    <li><a href="probono.php">PRO BONO</a></li>
+                    <li><a href="#">REPS</a></li>
+                    <li><a href="about.php">ABOUT</a></li>
+                    <li class="active"><a href="fahadscorner.php">FAHAD'S CORNER</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <img class="img-responsive" src="images/fahad2crop.jpg" width="980" height="307" />
+                <h1 class="law-header">Fahad&rsquo;s Corner</h1>
+
+                <p>
+                    Fahad is our lovely front-of-house guy, and we've given him his own page. Here you'll
+                    find lots of useful information which will help you settle in at UCL Laws and
+                    London. If there's anything you can't find, just ask Fahad at the front desk!
+                </p>
+
+                <h2>UCL Resources</h2>
+                <dl class="">
+                    <dt>Campus route finder</dt>
+                    <dd>
+                        <p>
+                            <a href="http://crf.casa.ucl.ac.uk/">http://crf.casa.ucl.ac.uk/</a> for when you need to get
+                            to the neuro-pychology building for that land-law lecture.
+                        </p>
+                    </dd>
+                    <dt>Campus Maps</dt>
+                    <dd>
+                        <p>
+                            Available from the official UCL student app, <strong>UCLGO!</strong> (for Apple, Android and
+                            Blackberry). Search for 'UCL' within the app store.
+                        </p>
+                    </dd>
+                    <dt>Twitter</dt>
+                    <dd>
+                        <p>
+                            <a href="https://twitter.com/UCLLaws">https://twitter.com/UCLLaws</a> - official Twitter account of the UCL Law Faculty
+                        </p>
+                        <p>
+                            <a href="https://twitter.com/UCLLawsociety">https://twitter.com/UCLLawsociety</a> - Law Soc Twitter account. More
+                            interesting that the Faculty one. <em>Allegedly</em>.
+                        </p>
+                    </dd>
+                </dl>
+
+
+                <h2>University of London Resources</h2>
+                <dl>
+                    <dt>UCLU <a href="www.uclu.org">www.uclu.org</a> </dt>
+                    <dd><p>The representative body for UCL students, founded in 1893.</p></dd>
+                    <dt>
+                        Senate House Library <a href="http://senatehouselibrary.ac.uk/">http://senatehouselibrary.ac.uk/</a>
+                    </dt>
+                    <dd>
+                        <p>
+                            The central library of the
+                            University of London. Free to join and lots of space to write essays. (Hint: the music library is best).
+                        </p>
+                    </dd>
+                </dl>
+            </div>
+            <div class="col-md-6">
+                <h2>Student Discounts</h2>
+                <dl>
+                    <dt>NUS Extra <a href="http://www.nus.org.uk/en/nus-extra/">http://www.nus.org.uk/en/nus-extra/</a> </dt>
+                    <dd><p>buy an NUS extra card and get lots of lovely discounts. 10% off at the Co-op (including the wine section)</p></dd>
+
+                    <dt>TfL <a href="https://tfl.gov.uk/fares-and-payments/adult-discounts-and-concessions/18-student">https://tfl.gov.uk/fares-and-payments/adult-discounts-and-concessions/18-student</a></dt>
+                    <dd><p> 30% off travelcards for UCL students.</p></dd>
+                </dl>
+
+                <h2>London resources</h2>
+                <dl>
+                    <dt>TimeOut London – <a href="http://www.timeout.com/london">http://www.timeout.com/london</a></dt>
+                    <dd><p>If a man is tired of London, he's clearly tired of reading the TimeOut listings section.</p></dd>
+                </dl>
+
+                <h2>Official Legal Resources</h2>
+                <dl>
+                    <dt>The Law Society - <a href="https://www.lawsociety.org.uk/">https://www.lawsociety.org.uk/</a></dt>
+                    <dt>Bar Standards Board - <a href="https://www.barstandardsboard.org.uk/">https://www.barstandardsboard.org.uk/</a></dt>
+                    <dt>Ministry of Justice - <a href="http://www.justice.gov.uk/">http://www.justice.gov.uk/</a> - boo hiss.</dt>
+                </dl>
+
+                <h2>Alternative legal Resources</h2>
+                <dl>
+                    <dt>Legal Cheek - <a href="http://www.legalcheek.com">http://www.legalcheek.com</a></dt>
+                    <dd><p>Legal news &amp; gossip, cheeky commentary and careers advice.</p></dd>
+                    <dt>Guardian student law - <a href="http://www.theguardian.com/education/law">http://www.theguardian.com/education/law</a></dt>
+                    <dd><p></p></dd>
+                    <dt>Alastair Hudson - <a href="http://www.alastairhudson.com/">http://www.alastairhudson.com/</a></dt>
+                    <dd>
+                        <p>
+                            Lectures and podcasts from leading Southampton University academic.
+                            Also an opportunity to see what a website looked like in 2003.
+                        </p>
+                    </dd>
+                </dl>
+
+
+            </div>
+        </div>
+    </div>
+    <footer class="footer navbar-inverse">
+        <div class="container">
+            <h2>&nbsp;</h2>
+            <p>&copy; UCL Law Society.</p>
+        </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+    <script>!function (d, s, id) { var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https'; if (!d.getElementById(id)) { js = d.createElement(s); js.id = id; js.src = p + '://platform.twitter.com/widgets.js'; fjs.parentNode.insertBefore(js, fjs); } }(document, 'script', 'twitter-wjs');</script>
+</body>
+</html>
